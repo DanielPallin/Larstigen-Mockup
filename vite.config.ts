e7a1692e7bcc7c4e779d6d6282a825cad81a1183
Daniel Pallin <danielpallin94@hotmail.com>
2026-04-05 03:11:27 +0200

    vite conf

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,23 @@
 import { resolve } from 'path';
-import { defineConfig } from 'vite';
+// Ändra denna rad från 'vite' till 'vitest/config'
+import { defineConfig } from 'vitest/config'; 
 
 export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        // Lista alla era HTML-filer här så Vite bygger dem
         main: resolve(__dirname, 'index.html'),
         dashboard: resolve(__dirname, 'pages/dashboard.html'),
-        barnprofil: resolve(__dirname, 'pages/barnprofil.html')
-        // Lägg till Sandras och Julias sidor här sen!
+        barnprofil: resolve(__dirname, 'pages/barnprofil.html'),
+        information: resolve(__dirname, 'pages/information.html'),
+        dagbok: resolve(__dirname, 'pages/dagbok.html'),
+        kalender: resolve(__dirname, 'pages/kalender.html')
       }
     }
+  },
+  test: {
+    include: ['tests/unit/**/*.test.js'],
+    environment: 'jsdom',
+    globals: true
   }
 });
